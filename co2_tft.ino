84e26e7f61eb8b542695c8e202f5e5277fb359a9
Karlduino <karlduino@gmail.com>
2022-09-21 21:48:56 -0500

    Revise to use SSL; now working!

--- co2_tft.ino
+++ co2_tft.ino
@@ -5,6 +5,36 @@
 #include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 #include <SPI.h>
 #include <WiFi.h>
+#include <WiFiMulti.h>
+#include <HTTPClient.h>
+#include <WiFiClientSecure.h>
+
+// google docs certificate
+const char* root_ca = \
+    "-----BEGIN CERTIFICATE-----\n" \
+    "MIIDdTCCAl2gAwIBAgILBAAAAAABFUtaw5QwDQYJKoZIhvcNAQEFBQAwVzELMAkG\n" \
+    "A1UEBhMCQkUxGTAXBgNVBAoTEEdsb2JhbFNpZ24gbnYtc2ExEDAOBgNVBAsTB1Jv\n" \
+    "b3QgQ0ExGzAZBgNVBAMTEkdsb2JhbFNpZ24gUm9vdCBDQTAeFw05ODA5MDExMjAw\n" \
+    "MDBaFw0yODAxMjgxMjAwMDBaMFcxCzAJBgNVBAYTAkJFMRkwFwYDVQQKExBHbG9i\n" \
+    "YWxTaWduIG52LXNhMRAwDgYDVQQLEwdSb290IENBMRswGQYDVQQDExJHbG9iYWxT\n" \
+    "aWduIFJvb3QgQ0EwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDaDuaZ\n" \
+    "jc6j40+Kfvvxi4Mla+pIH/EqsLmVEQS98GPR4mdmzxzdzxtIK+6NiY6arymAZavp\n" \
+    "xy0Sy6scTHAHoT0KMM0VjU/43dSMUBUc71DuxC73/OlS8pF94G3VNTCOXkNz8kHp\n" \
+    "1Wrjsok6Vjk4bwY8iGlbKk3Fp1S4bInMm/k8yuX9ifUSPJJ4ltbcdG6TRGHRjcdG\n" \
+    "snUOhugZitVtbNV4FpWi6cgKOOvyJBNPc1STE4U6G7weNLWLBYy5d4ux2x8gkasJ\n" \
+    "U26Qzns3dLlwR5EiUWMWea6xrkEmCMgZK9FGqkjWZCrXgzT/LCrBbBlDSgeF59N8\n" \
+    "9iFo7+ryUp9/k5DPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNVHRMBAf8E\n" \
+    "BTADAQH/MB0GA1UdDgQWBBRge2YaRQ2XyolQL30EzTSo//z9SzANBgkqhkiG9w0B\n" \
+    "AQUFAAOCAQEA1nPnfE920I2/7LqivjTFKDK1fPxsnCwrvQmeU79rXqoRSLblCKOz\n" \
+    "yj1hTdNGCbM+w6DjY1Ub8rrvrTnhQ7k4o+YviiY776BQVvnGCv04zcQLcFGUl5gE\n" \
+    "38NflNUVyRRBnMRddWQVDf9VMOyGj/8N7yy5Y0b2qvzfvGn9LhJIZJrglfCm7ymP\n" \
+    "AbEVtQwdpf5pLGkkeB6zpxxxYu7KyJesF12KwvhHhm4qxFYxldBniYUr+WymXUad\n" \
+    "DKqC5JlR3XC321Y9YeRq4VzW9v493kHMB65jUr9TU/Qr6cf9tveCX4XSQRjbgbME\n" \
+    "HMUfpIBvFSDJ3gyICh3WZlXi/EjJKSZp4A==\n"                            \
+    "-----END CERTIFICATE-----\n";
+
+WiFiClientSecure client;
+
 
 // private info
 #include "private.h"
@@ -19,10 +49,11 @@ bool wifi_connected=1;
 #define MS_TIMEOUT 5000
 unsigned long last_post_time=0;
 
-
 Adafruit_SCD30  scd30;
 // Use dedicated hardware SPI pins
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+unsigned long sessionID;
 
 void setup(void) {
   // Try to initialize!
@@ -52,6 +83,8 @@ void setup(void) {
       Serial.println(WiFi.localIP());
   }
 
+  client.setCACert(root_ca);
+
   // turn on backlite
   pinMode(TFT_BACKLITE, OUTPUT);
   digitalWrite(TFT_BACKLITE, HIGH);
@@ -69,6 +102,13 @@ void setup(void) {
   tft.init(135, 240); // Init ST7789 240x135
   tft.setRotation(3);
   tft.fillScreen(ST77XX_BLACK);
+
+  // create a random session ID
+  randomSeed(analogRead(0));
+  sessionID = random(1e20);
+  Serial.print("Session ID: ");
+  Serial.println(sessionID);
+
 }
 
 
@@ -116,14 +156,17 @@ void loop() {
     if(wifi_connected && (last_post_time==0 || millis() - last_post_time > MS_BETW_POSTS)) {
       last_post_time = millis();
 
-      WiFiClient client;
-      const int httpPort = 80;
+      const int httpPort = 443;
       if(!client.connect(api_host, httpPort)) {
+          Serial.println("Connection failed");
           return;
       }
+      Serial.println("Connected to server");
 
       // build up URI for request
-      String url = PRIVATE_API_CALL; // defined in private.h
+      String url = "https://";
+      url += api_host;
+      url += PRIVATE_API_CALL; // defined in private.h
       url += "&";
       url += PRIVATE_ENTRY1; // defined in private.h
       url += "=";
@@ -131,7 +174,7 @@ void loop() {
       url += "&";
       url += PRIVATE_ENTRY2; // defined in private.h
       url += "=";
-      url += "session001";
+      url += sessionID;
       url += "&";
       url += PRIVATE_ENTRY3; // defined in private.h
       url += "=";
@@ -148,7 +191,7 @@ void loop() {
       Serial.println();
       Serial.println(url);
 
-      client.print(String("Get ") + url + " HTTP/1.1\r\n" +
+      client.print(String("GET ") + url + " HTTP/1.1\r\n" +
                    "Host: " + api_host + "\r\n" +
                    "Connection: close\r\n\r\n");
       unsigned long timeout = millis();
@@ -162,11 +205,12 @@ void loop() {
 
       Serial.println();
       while(client.available()) {
-          String line = client.readStringUntil('\r');
-          Serial.println(line);
+          char c = client.read();
+          Serial.write(c);
       }
       Serial.println();
 
+      client.stop();
     } // end post form data
 
   delay(100);
